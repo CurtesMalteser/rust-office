bff17aa3e6f5c03179c2a46cc4b0d93731d9aab4
Curtes Malteser <antonio_bastiao@hotmail.com>
2025-07-12 11:01:33 +0200

    feat: Implement generate hello.docx
    - Intalled crate docx-rs version 0.4.7
    - Set resolver 2
    - Implemented some .docx styles to explore add content to the file

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,48 @@
+use docx_rs::*;
+
+fn heading1() -> (&'static str, Style) {
+    let style_id = "Heading1";
+    let style = Style::new(style_id, StyleType::Paragraph)
+        .size(32)
+        .bold()
+        .color("0000FF")
+        .text_alignment(TextAlignmentType::Center);
+    (style_id, style)
+}
+
+fn body_style() -> (&'static str, Style) {
+    let body_id = "Body";
+    let body_style = Style::new(body_id, StyleType::Paragraph)
+        .size(24);
+    (body_id, body_style)
+}
+
+pub fn hello() -> Result<(), DocxError> {
+    let path = std::path::Path::new("./hello.docx");
+
+    let (heading1_id, heading1) = heading1();
+    let (body_id ,body_style) = body_style();
+    
+    let file = std::fs::File::create(path).unwrap();
+    Docx::new()
+        .add_style(heading1)
+        .add_style(body_style)
+        .add_paragraph(
+            Paragraph::new()
+                .style(&heading1_id)
+                .add_run(
+                    Run::new()
+                        .add_text("Hello,")
+                    )
+                )
+        .add_paragraph(Paragraph::new()
+            .style(&body_id)
+            .add_run(Run::new().add_text("what's up?")))
+        .build()
+        .pack(file)?;
+    Ok(())
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -9,6 +54,7 @@ mod tests {
     #[test]
     fn it_works() {
         let result = add(2, 2);
+        let _ = hello();
         assert_eq!(result, 4);
     }
 }
