54f01fcfc7d27eec8f9f2cb08faf7b0a9e8f0d01
Curtes Malteser <antonio_bastiao@hotmail.com>
2025-07-15 22:05:58 +0200

    feat: Implement header photo + about me

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::Read;
 use docx_rs::*;
 
 fn heading1() -> (&'static str, Style) {
@@ -21,28 +23,36 @@ fn about_me() -> Table {
     // Add table.
     // Left cell with a picture of CV person
     // Right cell with a text box containing the CV information
+    let mut img = File::open("../images/bot.png").unwrap();
+    let mut buf = Vec::new();
+    let _ = img.read_to_end(&mut buf).unwrap();
+ 
+    // 9_525 EMUs per pixel 
+    // Width: 150 * 9525 = 1_428_750 EMUs
+    // Height: 100 * 9525 =   952_500 EMUs
+    let pic = Pic::new(&buf).size(150 * 9525, 100 * 9525);
 
     let cell : Vec<TableCell> = vec![
         TableCell::new()
             .add_paragraph(
                 Paragraph::new()
-                    .add_run(Run::new().add_text("Picture of CV person"))
+                    .add_run(Run::new().add_image(pic))
             ),
-            TableCell::new()
+        TableCell::new()
             .add_paragraph(
                 Paragraph::new()
-                    .add_run(Run::new().add_text("CV Information"))
-            )
+                    .add_run(Run::new().add_text("I'm a bot Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id purus et justo aliquam mollis at vitae nunc. Praesent semper condimentum euismod. Proin porttitor enim risus, sit amet sollicitudin diam eleifend at. Proin neque ligula, congue sed efficitur a, ornare tempor justo. Vestibulum eget auctor urna. Proin id massa blandit, volutpat eros a, volutpat tellus. Etiam in est et turpis consequat volutpat id blandit est. Praesent ultrices, dolor vitae congue dapibus, risus lacus fringilla diam, in malesuada diam est non arcu.").color("ffffff"))
+            ).shading(Shading::new().fill("007ACC")),
     ];
 
     let table_rows : Vec<TableRow> = vec![TableRow::new(cell)];
 
     Table::new(table_rows)
-        .set_grid(vec![1, 1])
+        .set_grid(vec![3000, 7000])
         .indent(0)
         .align(TableAlignmentType::Center)
-        .style("TableStyle")
-        .layout(TableLayoutType::Fixed)
+        .layout(TableLayoutType::Autofit)
+        .clear_all_border()
 
 }
 
@@ -54,9 +64,9 @@ pub fn hello() -> Result<(), DocxError> {
     
     let file = std::fs::File::create(path).unwrap();
     Docx::new()
+        .add_table(about_me())
         .add_style(heading1)
         .add_style(body_style)
-        .add_table(about_me())
         .add_paragraph(
             Paragraph::new()
                 .style(&heading1_id)
